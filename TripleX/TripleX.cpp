d4127941a5b3a60ecdca4c2079b8837b37054147
Matthew <matthew.t.mckenney@gmail.com>
2020-08-25 21:11:32 -0600

    updating

--- TripleX/TripleX.cpp
+++ TripleX/TripleX.cpp
@@ -5,10 +5,17 @@ using namespace std;
 
 int main() 
 {
+	int difficulty = 1;
+	int maxDifficulty = 10;
 
-
+	cout << endl;
 	cout << "You are a secret agent breaking into a secure server room...\n";
-	cout << "Enter the correct code to continue..." << endl;
+	cout << "Enter the correct code to continue...\n" << endl;
+
+	int a = 4, b = 3, c = 2, sum = a + b + c, product = a * b * c;
+
+	cout << "The sum of the three numbers is " << sum << ".\n";
+	cout << "The product of the three numbers is " << product << "." << endl;
 
 
 	return 0;
