8b356e082a4091257db096d8c0b798a802d92a37
Matthew <matthew.t.mckenney@gmail.com>
2020-08-25 18:16:29 -0600

    updating

--- TripleX/TripleX.cpp
+++ TripleX/TripleX.cpp
@@ -7,7 +7,8 @@ int main()
 {
 
 
-	cout << "hello" << endl;
+	cout << "You are a secret agent breaking into a secure server room...\n";
+	cout << "Enter the correct code to continue..." << endl;
 
 
 	return 0;
