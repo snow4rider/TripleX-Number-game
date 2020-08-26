5a463ba67b303a88b70ed7b28dd1e815f8eb2d37
Matthew <matthew.t.mckenney@gmail.com>
2020-08-25 22:17:20 -0600

    update

--- TripleX/TripleX.cpp
+++ TripleX/TripleX.cpp
@@ -2,15 +2,19 @@
 #include <ctime>
 using namespace std;
 
-
-int main() 
+void PrintIntroduction()
 {
-	int difficulty = 1;
-	int maxDifficulty = 10;
-
 	// Print welcome messages to the terminal
-	cout << "You are a secret agent breaking into a secure server room...\n";
-	cout << "Enter the correct code to continue...\n" << endl;
+	cout << "****************************************************************\n";
+	cout << "* You are a secret agent breaking into a secure server room... *\n";
+	cout << "* Enter the correct code to continue...                        *\n";
+	cout << "****************************************************************\n";
+}
+
+
+void PlayGame()
+{
+	PrintIntroduction();
 
 	const int CodeA = 4, CodeB = 3, CodeC = 2;
 	const int CodeSum = CodeA + CodeB + CodeC, CodeProduct = CodeA * CodeB * CodeC;
@@ -18,26 +22,34 @@ int main()
 	// Print the sum and product of the numbers
 	cout << "\t- There are three numbers in the code.\n";
 	cout << "\t- The sum of the three numbers is " << CodeSum << ".\n";
-	cout << "\t- The product of the three numbers is " << CodeProduct << "." << endl;
+	cout << "\t- The product of the three numbers is " << CodeProduct << ".\n";
 
 	int GuessA, GuessB, GuessC;
-
-	cin >> GuessA;
-	cin >> GuessB;
-	cin >> GuessC;
+	cin >> GuessA >> GuessB >> GuessC;
 
 	int GuessSum = GuessA + GuessB + GuessC;
 	int GuessProduct = GuessA * GuessB * GuessC;
 
+	// check if the player guessed correctly 
 	if (GuessSum == CodeSum && GuessProduct == CodeProduct)
 	{
-		cout << "you win";
+		cout << "you win\n";
 	}
 	else
 	{
-		cout << "you lose!";
+		cout << "you lose!\n";
 	}
+}
 
+
+
+
+int main() 
+{
+	int difficulty = 1;
+	int maxDifficulty = 10;
+
+	PlayGame();
 
 	return 0;
 }
